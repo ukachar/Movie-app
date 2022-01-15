1909b6abcaf94ceb2bedc5bde6d8dfd345dd95ca
ukachar <83542678+ukachar@users.noreply.github.com>
2022-01-15 11:39:32 +0100

    Dodani MODAL-i
    Dodana funkcija modala, dodan logo, dodana funkcija new search

--- app.js
+++ app.js
@@ -3,6 +3,10 @@ const search = document.getElementById("search");
 const srchbtn = document.getElementById("srchbtn");
 const moviesGroup = document.getElementById("movies-group");
 const IMGPATH = "https://image.tmdb.org/t/p/w1280";
+const hero = document.getElementById("hero");
+const nav = document.getElementById("navigacija");
+const searchDio = document.getElementById("searchDio");
+
 function searchFun() {
   fetch(
     `https://api.themoviedb.org/3/search/movie?api_key=${API}&query=${search.value}&page=1`
@@ -12,43 +16,73 @@ function searchFun() {
       console.log(data);
       console.log(data.results);
 
+      //Uklanjanje Hero-a nakon klika search i dodavanje NAV (VISIBILITY)
+      hero.classList.add("d-none");
+      searchDio.classList.add("d-none");
+      nav.classList.remove("d-none");
+
       data.results.forEach((element) => {
+        let id = element.id;
+
+        console.log(id);
+
+        //Kreiranje "CARD" elemenata sa filmovima
         moviesGroup.innerHTML += `  
         <div class="col mb-4">
         <div class="card">
+       <a data-bs-toggle="modal" data-bs-target="#modal${id}">
           <img src="${
             IMGPATH + element.poster_path
-          }" class="card-img-top" alt="..." />
+          }" class="card-img-top" id="movie_img${id}" alt="..." style="cursor: pointer;" />
+         </a>
           <div class="card-body">
-            <h5 class="card-title">Card title</h5>
-            <p class="card-text">
-              This is a longer card with supporting text below as a natural
-              lead-in to additional content. This content is a little bit
-              longer.
+            <h5 class="card-title">${element.original_title}</h5>
+            <p class="card-text">${provjeraDuljine(element.overview)}
             </p>
           </div>
         </div>
       </div>
             `;
+
+        //Kreiranje "MODAL" elemenata za svaki film sa detaljnim informacijama
+        moviesGroup.innerHTML += `
+            <div class="modal fade" id="modal${id}" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+              <div class="modal-dialog modal-dialog-centered">
+                <div class="modal-content">
+                  <div class="modal-header">
+                    <h5 class="modal-title" id="exampleModalLabel">${
+                      element.original_title
+                    }</h5>
+                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                    </div>
+                    <div class="modal-body">
+                    <img src="${
+                      IMGPATH + element.backdrop_path
+                    }" class="card-img-top" id="movie_img${id}" alt="..." style="cursor: pointer;" />
+                    <p>Release date: ${element.release_date}</p>
+                     <p style="font-weight:bold;">Overview:</p>
+                     <p>${element.overview}</p>
+                    </div>
+                    <div class="modal-footer">
+                    <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+                  </div>
+                </div>
+              </div>
+          </div>
+            `;
       });
     });
 }
-/*
-const results = {
-  poster_path: "/cezWGskPY5x7GaglTTRN4Fugfb8.jpg",
-  adult: false,
-  overview:
-    "When an unexpected enemy emerges and threatens global safety and security, Nick Fury, director of the international peacekeeping agency known as S.H.I.E.L.D., finds himself in need of a team to pull the world back from the brink of disaster. Spanning the globe, a daring recruitment effort begins!",
-  release_date: "2012-04-25",
-  genre_ids: [878, 28, 12],
-  id: 24428,
-  original_title: "The Avengers",
-  original_language: "en",
-  title: "The Avengers",
-  backdrop_path: "/hbn46fQaRmlpBuUrEiFqv0GDL6Y.jpg",
-  popularity: 7.353212,
-  vote_count: 8503,
-  video: false,
-  vote_average: 7.33,
-};
-*/
+
+function searchAgain() {
+  moviesGroup.innerHTML = "";
+  hero.classList.remove("d-none");
+  searchDio.classList.remove("d-none");
+  nav.classList.add("d-none");
+}
+
+function provjeraDuljine(tekst) {
+  let a = tekst.substring(tekst, 100);
+  let b = (a += "...");
+  return b;
+}
